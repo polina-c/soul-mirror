3376e470fcd6c06db7b630e82ef92eb552b6025c
Polina Cherkasova <polina.c@live.com>
2026-02-22 12:11:33 -0800

    Update download.dart

--- pkgs/shared/lib/src/download/download.dart
+++ pkgs/shared/lib/src/download/download.dart
@@ -1,6 +1,5 @@
 import 'dart:typed_data';
 
-import 'io_download_image.dart'
-    if (dart.library.html) 'web_download_image.dart';
+import 'io_download.dart' if (dart.library.html) 'web_download.dart';
 
 Future<void> download(Uint8List bytes) => platformDownload(bytes);
